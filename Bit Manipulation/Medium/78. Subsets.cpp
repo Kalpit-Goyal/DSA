3a3bfe44835dc46fa6b360cfd1699eca42d79927
Kalpit Goyal <146472173+Kalpit-Goyal@users.noreply.github.com>
2026-04-12 23:56:25 +0530

    Add solution for generating subsets of a set
    Implement subset generation using bit manipulation.

---
+++ Bit Manipulation/Medium/78. Subsets.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/subsets/description/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int subs=1<<(nums.size());
+        vector<vector<int>> ans;
+
+        for(int i=0;i<subs;i++){
+            vector<int> subset;
+            for(int j=0;j<nums.size();j++){
+                if(i&(1<<j)){
+                    subset.push_back(nums[j]);
+                }
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+        
+    }
+};
